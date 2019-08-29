fc76492602a2702ad7c22d931802c0a69e65afd6
KentangRebus <steven.ferdianto0110@gmail.com>
2019-08-29 11:35:35 +0700

    xss fixed

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -37,7 +37,7 @@ class AnnouncementController extends Controller
     public function store(Request $request)
     {
         $data = new Announcement();
-        $data->announcement = $request->announcement;
+        $data->announcement = htmlentities($request->announcement);
 
         $data->save();
 
