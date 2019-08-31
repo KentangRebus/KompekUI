434cd647340a51d9ffea9e5207476945cacf9c02
KentangRebus <steven.ferdianto0110@gmail.com>
2019-08-31 15:16:56 +0700

    Update adminanswer.blade.php
    Front End Done

--- resources/views/adminanswer.blade.php
+++ resources/views/adminanswer.blade.php
@@ -9,10 +9,43 @@
 @stop
 
 @section('adminContent')
-    <div>
+    <div class="pl-4 pr-2">
         <h1 class="text-center mt-3">Manage Answer</h1>
+        <div class="mt-5 mb-3">
+            <form action="">
+                <button type="submit" class="btn btn-primary">Download All Answer</button>
+            </form>
+        </div>
+        <table class="table table-hover">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">No.</th>
+                <th scope="col">School</th>
+                <th scope="col">Head Name</th>
+                <th scope="col">Head Email</th>
+                <th scope="col">Competition</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>BINUS</td>
+                    <td>Budi</td>
+                    <td>budi@gmail.com</td>
+                    <td>EQ</td>
+                    <td>
+                        <form action="" class="mb-1">
+                            <button type="button" class="btn btn-outline-danger" style="min-width: 90px">Delete</button>
+                        </form>
+                        <form action="">
+                            <button type="button" class="btn btn-outline-success">Download</button>
+                        </form>
+                    </td>
+                </tr>
 
-
+            </tbody>
+        </table>
 
     </div>
 @stop
